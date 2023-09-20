90982c8d29fe1f34400eab53b39e4bf0407c89ad
asabya <asabya@gmail.com>
2023-09-20 15:31:08 +0530

    update api

--- fave.py
+++ fave.py
@@ -1,8 +1,8 @@
 from typing import List
 
-import fave_swagger_client.swagger_client as swagger_client
-from fave_swagger_client.swagger_client.rest import ApiException
-from fave_swagger_client.swagger_client.models import Index, Document
+import fave_api
+from fave_api.rest import ApiException
+from fave_api.models import Index, Document
 
 
 class FaVe():
@@ -12,15 +12,15 @@ class FaVe():
     ) -> None:
         self._url = url
 
-        configuration = swagger_client.Configuration()
+        configuration = fave_api.Configuration()
         configuration.host = url+"/v1"
 
-        self._client = swagger_client.DefaultApi(swagger_client.ApiClient(configuration))
+        self._client = fave_api.DefaultApi(fave_api.ApiClient(configuration))
         
     def create_collection(
         self, collection: str, indexes: list[Index]
     ) -> None:
-        colection = swagger_client.Collection()
+        colection = fave_api.Collection()
         colection.name = collection
         colection.indexes = indexes
         try:
@@ -29,10 +29,14 @@ class FaVe():
             raise Exception("%s\n" % e)
         
     def add_documents(
-        self, collection: str, documents: list[Document]
+        self, collection: str, documents: list[Document], properties_to_vectorize: list[str]
     ) -> None:
+        rqst = fave_api.AddDocumentsRequest()
+        rqst.name = collection
+        rqst.documents = documents
+        rqst.properties_to_vectorize = properties_to_vectorize
         try:
-            self._client.fave_add_documents(collection, documents)
+            self._client.fave_add_documents(rqst)
         except ApiException as e:
             raise Exception("%s\n" % e)
         
@@ -47,7 +51,7 @@ class FaVe():
     def get_nearest_documents(
         self, collection, query: str, limit: int, distance: float
     ) -> List[Document]:
-        rqst = swagger_client.NearestDocumentsRequest()
+        rqst = fave_api.NearestDocumentsRequest()
         rqst.name = collection
         rqst.text = query
         rqst.limit = limit
@@ -62,7 +66,7 @@ class FaVe():
     def get_nearest_documents_by_vector(
         self, collection: str, vector: list[float],limit: int, distance: float
     ) -> List[Document]:
-        rqst = swagger_client.NearestDocumentsByVectorRequest()
+        rqst = fave_api.NearestDocumentsByVectorRequest()
         rqst.name = collection
         rqst.vector = vector
         rqst.limit = limit
